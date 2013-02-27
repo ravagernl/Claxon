b6051abe6ccf02f9dac86077776c5eb3ce64a655
Ruben Vincenten <rubenvincenten@gmail.com>
2013-02-27 20:21:41 +0100

    Initial import from wow interface version

---
+++ Claxon.lua
@@ -0,0 +1,39 @@
+local AddOnName, ns = ...
+
+local mediaPath = [=[Interface\AddOns\Claxon\media\]=]
+
+local print = function(...)
+	print(AddOnName, ': ', ...)
+end
+
+local name = UnitName'player'
+
+local evtsounds = {
+	["CHAT_MSG_GUILD"] = "Kachink",
+	["CHAT_MSG_OFFICER"] = "Link",
+	["CHAT_MSG_PARTY"] = "pop1",
+	["CHAT_MSG_PARTY_LEADER"] = "pop2",
+	["CHAT_MSG_RAID"] = "Text2",
+	["CHAT_MSG_WHISPER"] = "Heart",
+	["CHAT_MSG_GMWHISPER"] = "gasp",
+	["CHAT_MSG_RAID_LEADER"] = "Choo",
+	["CHAT_MSG_BATTLEGROUND"] = "switchy",
+	["CHAT_MSG_BATTLEGROUND_LEADER"] = "doublehit",
+	["CHAT_MSG_CHANNEL"] = true, --dummy
+}
+
+local f = CreateFrame'Frame'
+f:SetScript('OnEvent', function(self, event, ...)
+	local msg, author, lang, channel = ...
+	if author == name then return end
+	if event == "CHAT_MSG_CHANNEL" then
+		-- TODO
+	else
+		local sound = mediaPath..evtsounds[event]..'.mp3'
+		PlaySoundFile(sound)
+	end
+end)
+
+for event, sound in pairs(evtsounds) do
+	f:RegisterEvent(event)
+end
